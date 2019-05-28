376170455b5d66aa7c7e8edde0fcf276d4416bff
Dario Kemenovic <dkemenovic@gmail.com>
2019-05-28 10:32:05 +0200

    Add necessary elements

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+//import logo from './logo.svg';
+import './App.css';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <span>Picture:</span>
+        <input />
+
+        <span>Name:</span>
+        <input />
+
+        <button>Add Friend</button>
+      </div>
+    );
+  }
+}
+
+export default App;
